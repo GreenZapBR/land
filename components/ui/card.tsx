f5eb53791182bc85ed098211bbf28d592b067b6c
GreenZapBR <contato.greenzap@gmail.com>
2025-06-19 15:53:17 -0300

    Delete components directory

--- components/ui/card.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Card({ children, className = "" }: any) {
-  return <div className={`rounded-xl bg-[#121212] p-4 shadow ${className}`}>{children}</div>;
-}
-
-export function CardContent({ children, className = "" }: any) {
-  return <div className={className}>{children}</div>;
-}
