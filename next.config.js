274e3d9fb4a863107e253a5e963596cbffa0c8dd
GreenZapBR <contato.greenzap@gmail.com>
2025-06-19 15:52:48 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true, // garante que o App Router será usado
-  },
-};
-
-module.exports = nextConfig;
