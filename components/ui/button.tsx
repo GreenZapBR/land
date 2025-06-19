893d0634df4b34a71759ee24a01a3b1e1ac42c0c
GreenZapBR <contato.greenzap@gmail.com>
2025-06-19 16:44:08 -0300

    Delete components directory

--- components/ui/button.tsx
+++
@@ -1,8 +0,0 @@
-
-export function Button({ children, className = "", ...props }: any) {
-  return (
-    <button className={\`rounded px-4 py-2 font-medium transition-all \${className}\`} {...props}>
-      {children}
-    </button>
-  );
-}
