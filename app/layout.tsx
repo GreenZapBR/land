3b53a3867bac9875da7e39aa52e4e0797e177651
GreenZapBR <contato.greenzap@gmail.com>
2025-06-19 16:18:21 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
       <head />
-      <body>{children}</body>
+      <body className="bg-black text-white min-h-screen">{children}</body>
     </html>
   );
 }
