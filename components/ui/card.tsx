2eb9cc512f499c1bd7311bd3fa47354fd5c53a9d
GreenZapBR <contato.greenzap@gmail.com>
2025-06-19 16:31:14 -0300

    Add files via upload

---
+++ components/ui/card.tsx
@@ -0,0 +1,8 @@
+
+export function Card({ children, className = "" }: any) {
+  return <div className={\`rounded-xl bg-[#121212] p-4 shadow \${className}\`}>{children}</div>;
+}
+
+export function CardContent({ children, className = "" }: any) {
+  return <div className={className}>{children}</div>;
+}
